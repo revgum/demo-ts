0f61436229c6633792230c5ebc4e3144ad187fc0
Josh Gum <josh.gum@sos.oregon.gov>
2025-02-06 10:53:38 -0800

    lint fixes

--- app/webapp/main.ts
+++ app/webapp/main.ts
@@ -1,35 +1,31 @@
-import express, { type Request, type Response } from "express";
-import { DaprClient, HttpMethod } from "@dapr/dapr";
+import { DaprClient, HttpMethod } from '@dapr/dapr';
+import express, { type Request, type Response } from 'express';
 
 // Create a new express application instance
 const app = express();
 
 // Set the network port
 const port = process.env.PORT || 3000;
-const daprHost = process.env.DAPR_HOST || "127.0.0.1";
-const daprPort = process.env.DAPR_PORT || "3500";
+const daprHost = process.env.DAPR_HOST || '127.0.0.1';
+const daprPort = process.env.DAPR_PORT || '3500';
 
 // Define the root path with a greeting message
-app.get("/", async (req: Request, res: Response) => {
-	const client = new DaprClient({ daprHost, daprPort });
+app.get('/', async (req: Request, res: Response) => {
+  const client = new DaprClient({ daprHost, daprPort });
 
-	const serviceAppId = "backend";
-	const serviceMethod = "hello-world";
+  const serviceAppId = 'backend';
+  const serviceMethod = 'hello-world';
 
-	// GET Request
-	const response = await client.invoker.invoke(
-		serviceAppId,
-		serviceMethod,
-		HttpMethod.GET,
-	);
+  // GET Request
+  const response = await client.invoker.invoke(serviceAppId, serviceMethod, HttpMethod.GET);
 
-	res.json({
-		message: "Welcome to the Express + TypeScript Server!",
-		response,
-	});
+  res.json({
+    message: 'Welcome to the Express + TypeScript Server!',
+    response,
+  });
 });
 
 // Start the Express server
 app.listen(port, () => {
-	console.log(`The server is running at http://localhost:${port}`);
+  console.log(`The server is running at http://localhost:${port}`);
 });
