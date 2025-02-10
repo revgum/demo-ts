a1d0f2cbebf0257e0f6cc585fad7bd3534be7e82
Josh Gum <josh.gum@sos.oregon.gov>
2025-02-10 14:09:25 -0800

    simplifying test service call

--- app/web-nuxt/server/services/test.ts
+++ app/web-nuxt/server/services/test.ts
@@ -3,24 +3,12 @@ import { context } from '../context';
 import type { Test } from '../types';
 import { METHODS } from './constants';
 
-const SERVICE_APP_ID = 'backend';
+const SERVICE_APP_ID = 'backend-ts';
 
-export const getById = async (id: Test['id']) => {
-  const [test, test_dotnet] = await Promise.all([
-    context.dapr.invoker.invoke(SERVICE_APP_ID, METHODS.TestGetById, HttpMethod.POST, { id }),
-  ]);
-  console.log({ test }, { test_dotnet });
-  return test;
-};
+export const getById = async (id: Test['id']) =>
+  context.dapr.invoker.invoke(SERVICE_APP_ID, METHODS.TestGetById, HttpMethod.POST, { id });
 
-export const getAll = async () => {
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  const [tests, tests_dotnet]: any[] = await Promise.all([
-    context.dapr.invoker.invoke(SERVICE_APP_ID, METHODS.TestGetAll, HttpMethod.GET),
-  ]);
-  console.log(`backend returned: ${tests?.payload.length}, backend-node returned: ${tests_dotnet?.payload.length}`);
-  return tests;
-};
+export const getAll = async () => context.dapr.invoker.invoke(SERVICE_APP_ID, METHODS.TestGetAll, HttpMethod.GET);
 
 export const create = async (data: Partial<Test>) =>
   context.dapr.invoker.invoke(SERVICE_APP_ID, METHODS.TestCreate, HttpMethod.POST, data);
