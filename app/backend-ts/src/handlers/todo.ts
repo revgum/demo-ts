f646c8f223d2c68ee633b3c5f71ea7dee1ffb651
Josh Gum <josh.gum@sos.oregon.gov>
2025-02-21 11:05:27 -0800

    adds otel for backend-ts

--- app/backend-ts/src/handlers/todo.ts
+++ app/backend-ts/src/handlers/todo.ts
@@ -1,4 +1,5 @@
 import { type DaprInvokerCallbackContent, HttpMethod } from '@dapr/dapr';
+import opentelemetry from '@opentelemetry/api';
 import {
   create,
   deleteById as deleteTodoById,
@@ -9,13 +10,26 @@ import {
 import type { Context, ServiceRoutes, Todo } from '../types';
 import { METHODS } from './constants';
 
+const meter = opentelemetry.metrics.getMeter('todo-handler', '1.0.0');
+const counters = {
+  get: meter.createCounter('todo.getall'),
+  getById: meter.createCounter('todo.get'),
+  post: meter.createCounter('todo.create'),
+  updateById: meter.createCounter('todo.update'),
+  deleteById: meter.createCounter('todo.delete'),
+};
+
 const get = async (context: Context, _data: DaprInvokerCallbackContent): Promise<{ payload: Todo[] }> => {
+  let success = false;
   try {
     const rows = await getAll(context);
+    success = true;
     return { payload: rows };
   } catch (error) {
     console.error(error);
     return { payload: [] };
+  } finally {
+    counters.get.add(1, { success });
   }
 };
 
@@ -23,14 +37,18 @@ const post = async (
   context: Context,
   data: DaprInvokerCallbackContent,
 ): Promise<{ payload?: Todo; error?: string }> => {
+  let success = false;
   try {
     //TODO: Use Zod to validate/transform
     const { data: newTodo } = JSON.parse(data.body) as { data: Partial<Todo> };
     const todo = await create(context, newTodo);
+    success = true;
     return { payload: todo };
   } catch (error) {
     console.error(error);
     return { error: error.detail || 'Unhandled error' };
+  } finally {
+    counters.post.add(1, { success });
   }
 };
 
@@ -38,14 +56,18 @@ const getById = async (
   context: Context,
   data: DaprInvokerCallbackContent,
 ): Promise<{ payload?: Todo; error?: string }> => {
+  let success = false;
   try {
     //TODO: Use Zod to validate/transform
     const { id } = JSON.parse(data.body) as { id: string };
     const todo = await getTodoById(context, id);
+    success = true;
     return { payload: todo };
   } catch (error) {
     console.error(error);
     return { error: error.detail || 'Unhandled error' };
+  } finally {
+    counters.getById.add(1, { success });
   }
 };
 
@@ -53,14 +75,18 @@ const updateById = async (
   context: Context,
   data: DaprInvokerCallbackContent,
 ): Promise<{ payload?: Todo; error?: string }> => {
+  let success = false;
   try {
     //TODO: Use Zod to validate/transform
     const { id, data: updatedTodo } = JSON.parse(data.body) as { id: string; data: Partial<Todo> };
     const todo = await updateTodoById(context, id, updatedTodo);
+    success = true;
     return { payload: todo };
   } catch (error) {
     console.error(error);
     return { error: error.detail || 'Unhandled error' };
+  } finally {
+    counters.updateById.add(1, { success });
   }
 };
 
@@ -68,14 +94,18 @@ const deleteById = async (
   context: Context,
   data: DaprInvokerCallbackContent,
 ): Promise<{ payload?: Todo; error?: string }> => {
+  let success = false;
   try {
     //TODO: Use Zod to validate/transform
     const { id } = JSON.parse(data.body) as { id: string };
     const todo = await deleteTodoById(context, id);
+    success = true;
     return { payload: todo };
   } catch (error) {
     console.error(error);
     return { error: error.detail || 'Unhandled error' };
+  } finally {
+    counters.deleteById.add(1, { success });
   }
 };
 
